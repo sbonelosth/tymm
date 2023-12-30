4df1be18877231502c337f683530934d3c0434f8
sbonelosth <sbonelosthole97@gmail.com>
2023-12-30 09:21:14 +0200

    fixed the wrong current day selection bug

--- calender_script.js
+++ calender_script.js
@@ -54,6 +54,8 @@ function showCalendar(month, year) {
 
 	let firstDay = (new Date(year, month)).getDay();
 	let monthDays = 32 - new Date(year, month, 32).getDate();
+	let prevMonthDays = 32 - new Date(year, month - 1, 32).getDate();
+	let nextMonthStart = (new Date(year, month + 1)).getDay();
 
 	// Body of the calendar.
 	let tbl = document.getElementById("calendar-body");
@@ -67,7 +69,7 @@ function showCalendar(month, year) {
 	selectMonth.value = month;
 
 	let eventLabel = q(".event-label");
-	let quote = otherDays[Math.floor(Math.random() * otherDays.length)]
+	let quote = otherDays[Math.floor(Math.random() * otherDays.length)];
 
 	// Creating all cells.
 	let date = 1;
@@ -78,172 +80,179 @@ function showCalendar(month, year) {
 
 		// Creating individual cells & filling them up with data.
 		for (let j = 0; j < 7; j++) {
+			let cell = document.createElement("td");
+			let cellTxt;
+
 			if (i === 0 && j < firstDay) {
-				let cell = document.createElement("td");
-				let cellTxt = document.createTextNode("*");
-				cell.appendChild(cellTxt);
-				row.appendChild(cell);
+				cellTxt = document.createTextNode(prevMonthDays - firstDay + j + 1);
+				cell.classList.add("prev-month");
 			}
 			else if (date > monthDays) {
-				break;
+				if (i < 5) {
+					cellTxt  = document.createTextNode(date++ - monthDays);
+					cell.classList.add("next-month");
+				} else if (nextMonthStart < 2 && nextMonthStart > 0) {
+					cellTxt  = document.createTextNode(date++ - monthDays);
+					cell.classList.add("next-month");
+				}
+				else break;
 			}
 			else {
-				let cell = document.createElement("td")
-				let cellTxt = document.createTextNode(date)
 				function addlabel(eventString) {
-					eventLabel.innerHTML = eventString
-				}
+                    eventLabel.innerHTML = eventString
+                }
 
-				// Selecting events or holidays and adding labels to them
-				if (date === today.getDate() && month === today.getMonth()) {
-					switch (today.getMonth()) {
-						// January events
-						case 0:
-							switch (today.getDate()) {
-								case 1:
-									addlabel("Happy New Year, make it count."); break;
-								case 21:
-									addlabel("It's Rapsody's birthday today"); break;
-								default:
-									addlabel(quote)
-							}
-							break;
-						// February events
-						case 1:
-							switch (today.getDate()) {
-								case 14:
-									addlabel("Valentine's day"); break;
-								case 23:
-									addlabel("The day Little Simz was born"); break;
-								default:
-									addlabel(quote)
-							}
-							break;
-						// March events
-						case 2:
-							switch (today.getDate()) {
-								case 21:
-									addlabel("Human Rights day"); break;
-								case 31:
-									addlabel("End of the first quarter"); break;
-								default:
-									addlabel(quote)
-							}
-							break;
-						// April events
-						case 3:
-							switch (today.getDate()) {
-								case 27:
-									addlabel("Freedom day"); break;
-								default:
-									addlabel(quote)
-							}
-							break;
-						// May events labels
-						case 4:
-							switch (today.getDate()) {
-								case 1:
-									addlabel("Worker's day!"); break;
-								default:
-									addlabel(quote)
-							}
-							break;
-						// June events
-						case 5:
-							switch (today.getDate()) {
-								case 13:
-									addlabel("Dani's birthday"); break;
-								case 16:
-									addlabel("Youth day"); break;
-								case 30:
-									addlabel("Halfway through the year"); break;
-								default:
-									addlabel(quote)
-							}
-							break;
-						// July, August events
-						case 7:
-							switch (today.getDate()) {
-								case 9:
-									addlabel("Women's day"); break;
-								default:
-									addlabel(quote)
-							}
-							break;
-						// September events
-						case 8:
-							switch (today.getDate()) {
-								case 30:
-									addlabel("Into the final quarter"); break;
-								default:
-									addlabel(quote)
-							}
-							break;
-						// November events
-						case 10:
-							switch (today.getDate()) {
-								case 1:
-									addlabel("Continue to rest in peace Kirshnik. You're forever in our hearts."); break;
-								case 2:
-									addlabel(`The Last Rocket by Takeoff, turns ${currentYear - 2018} today.`); break;
-								case 13:
-									addlabel(`LIYAZONGOMA! turns ${currentYear - 2022} today.`); break;
-								default:
-									addlabel(quote)
-							}
-							break;
-						// December events
-						case 11:
-							switch (today.getDate()) {
-								case 16:
-									addlabel("A Day of Reconciliation!"); break;
-								case 24:
-									addlabel("Christmas eve"); break;
-								case 25:
-									addlabel("Merry Christmas"); break;
-								case 26:
-									addlabel("A Day of Goodwill"); break;
-								case 31:
-									addlabel("New Year's eve, enjoy responsibly."); break;
-								default:
-									addlabel(quote)
-							}
-							break;
-						default:
-							addlabel(quote)
-					}
-					cell.classList.add("today-bg");
-				}
-				// Adding class "event" to select and highlight the dates with events or holidays
-				// January events.
-				else if (date === 1 && month === 0) {
-					cell.classList.add("event");
-				}
-				// February, March events
-				else if (date === 21 && month === 2) {
-					cell.classList.add("event");
-				}
-				// April events
-				else if (date === 27 && month === 3) {
-					cell.classList.add("event");
-				}
-				// May, June events.
-				else if ((date === 1 && month === 4) || (date === 16 && month === 5)) {
-					cell.classList.add('event');
-				}
+                // Selecting events or holidays and adding labels to them
+                if (date === today.getDate() && month === today.getMonth()) {
+                    switch (today.getMonth()) {
+                        // January events
+                        case 0:
+                            switch (today.getDate()) {
+                                case 1:
+                                    addlabel("Happy New Year, make it count."); break;
+                                case 21:
+                                    addlabel("It's Rapsody's birthday today"); break;
+                                default:
+                                    addlabel(quote)
+                            }
+                            break;
+                        // February events
+                        case 1:
+                            switch (today.getDate()) {
+                                case 14:
+                                    addlabel("Valentine's day"); break;
+                                case 23:
+                                    addlabel("The day Little Simz was born"); break;
+                                default:
+                                    addlabel(quote)
+                            }
+                            break;
+                        // March events
+                        case 2:
+                            switch (today.getDate()) {
+                                case 21:
+                                    addlabel("Human Rights day"); break;
+                                case 31:
+                                    addlabel("End of the first quarter"); break;
+                                default:
+                                    addlabel(quote)
+                            }
+                            break;
+                        // April events
+                        case 3:
+                            switch (today.getDate()) {
+                                case 27:
+                                    addlabel("Freedom day"); break;
+                                default:
+                                    addlabel(quote)
+                            }
+                            break;
+                        // May events labels
+                        case 4:
+                            switch (today.getDate()) {
+                                case 1:
+                                    addlabel("Worker's day!"); break;
+                                default:
+                                    addlabel(quote)
+                            }
+                            break;
+                        // June events
+                        case 5:
+                            switch (today.getDate()) {
+                                case 13:
+                                    addlabel("Dani's birthday"); break;
+                                case 16:
+                                    addlabel("Youth day"); break;
+                                case 30:
+                                    addlabel("Halfway through the year"); break;
+                                default:
+                                    addlabel(quote)
+                            }
+                            break;
+                        // July, August events
+                        case 7:
+                            switch (today.getDate()) {
+                                case 9:
+                                    addlabel("Women's day"); break;
+                                default:
+                                    addlabel(quote)
+                            }
+                            break;
+                        // September events
+                        case 8:
+                            switch (today.getDate()) {
+                                case 30:
+                                    addlabel("Into the final quarter"); break;
+                                default:
+                                    addlabel(quote)
+                            }
+                            break;
+                        // November events
+                        case 10:
+                            switch (today.getDate()) {
+                                case 1:
+                                    addlabel("Continue to rest in peace Kirshnik. You're forever in our hearts."); break;
+                                case 2:
+                                    addlabel(`The Last Rocket by Takeoff, turns ${currentYear - 2018} today.`); break;
+                                case 13:
+                                    addlabel(`LIYAZONGOMA! turns ${currentYear - 2022} today.`); break;
+                                default:
+                                    addlabel(quote)
+                            }
+                            break;
+                        // December events
+                        case 11:
+                            switch (today.getDate()) {
+                                case 16:
+                                    addlabel("A Day of Reconciliation!"); break;
+                                case 24:
+                                    addlabel("Christmas eve"); break;
+                                case 25:
+                                    addlabel("Merry Christmas"); break;
+                                case 26:
+                                    addlabel("A Day of Goodwill"); break;
+                                case 31:
+                                    addlabel("New Year's eve, enjoy responsibly."); break;
+                                default:
+                                    addlabel(quote)
+                            }
+                            break;
+                        default:
+                            addlabel(quote)
+                    }
+                    cell.classList.add("current-day");
+                }
+                // Adding class "event" to select and highlight the dates with events or holidays
+                // January events.
+                else if (date === 1 && month === 0) {
+                    cell.classList.add("event");
+                }
+                // February, March events
+                else if (date === 21 && month === 2) {
+                    cell.classList.add("event");
+                }
+                // April events
+                else if (date === 27 && month === 3) {
+                    cell.classList.add("event");
+                }
+                // May, June events.
+                else if ((date === 1 && month === 4) || (date === 16 && month === 5)) {
+                    cell.classList.add('event');
+                }
 
-				// July, August, September events.
-				else if ((date === 9 && month === 7) || (date === 24 && month === 8)) {
-					cell.classList.add("event");
-				}
-				// December events
-				else if ((date === 16 || date === 25 || date === 26) && month === 11) {
-					cell.classList.add("event");
-				}
-				cell.appendChild(cellTxt);
-				row.appendChild(cell);
-				date++;
+                // July, August, September events.
+                else if ((date === 9 && month === 7) || (date === 24 && month === 8)) {
+                    cell.classList.add("event");
+                }
+                // December events
+                else if ((date === 16 || date === 25 || date === 26) && month === 11) {
+                    cell.classList.add("event");
+                }
+				cellTxt = document.createTextNode(date++);
 			}
+			cell.appendChild(cellTxt);
+			cell.classList.add(".otherdays")
+			row.appendChild(cell);
 		}
 		// Finally, appending each row into calendar body.
 		tbl.appendChild(row);
